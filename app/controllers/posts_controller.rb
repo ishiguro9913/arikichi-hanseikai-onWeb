8e14b6020d8fe43fb887fda81461c4e6a266713b
ishiguro tasuku <ishitasukuro9913@gmail.com>
2022-05-30 22:09:56 +0900

    Add: ツイートの文章を取得するところまで実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,17 +18,20 @@ class PostsController < ApplicationController
 
     # テスト用に禊文章をいれておく
     @post.ablution = 'テスト用の禊文章です'
-    @post.get_ablution
+    # @post.get_ablution
 
     # 投稿者の名前を入れる
     @post.name = current_user.name
 
-    @post.twitter_client
-
-    # @post.get_tweet
-
-    # @post = twitter_client.user_timeline(user_id: id, count: 1, exclude_replies: false, include_rts: false, contributor_details: false, result_type: "recent", locale: "ja", tweet_mode: "extended")
-
+    twitter_client
+    # binding.pry
+    # puts twitter_client.user_timeline(current_user.twitter_id, count: 200, tweet_mode: 'extended')
+    # tweet = twitter_client.user_timeline(user_id: current_user.twitter_id, count: 1, exclude_replies: false, include_rts: false, contributor_details: false, result_type: "recent", locale: "ja", tweet_mode: "extended")
+    twitter_client.user_timeline({ count: 200 }).each do |tweet|
+      puts "#{tweet.user.name}[ID:#{tweet.user.screen_name}]"
+      puts tweet.full_text
+    end
+    
     if @post.save
       redirect_to result_path(@post.id), success: '投稿が成功しました。'
     else
@@ -45,11 +48,11 @@ class PostsController < ApplicationController
 
   def twitter_client
     @twitter_client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key = ENV['CONSUMER_KEY']
-      config.consumer_secret = ENV['CONSUMER_SECRET']
+      config.consumer_key = ENV['TWITTER_API_KEY']
+      config.consumer_secret = ENV['TWITTER_API_SECRET']
       config.access_token = ENV['ACCESS_TOKEN']
-      config.access_token_secret = ENV['ACCESS_SECRET']
-      
+      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+      # binding.pry
     end
   end
 
