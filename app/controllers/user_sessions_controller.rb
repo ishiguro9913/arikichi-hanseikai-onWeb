c5517d0f72962c4aa643736b26e4fe1f1b360d0b
ishiguro tasuku <ishitasukuro9913@gmail.com>
2022-06-13 20:59:37 +0900

    user_sessions_controller.rb #17 ゲストログイン方法を修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,10 +1,7 @@
 class UserSessionsController < ApplicationController
 
   def guest_login
-    @guest_user = User.create(
-    twitter_id: nil,  
-    name: '匿名様',
-    )
+    @guest_user = User.find(1)
     auto_login(@guest_user)
     redirect_to new_post_path, success: 'ゲストとしてログインしました'
   end
